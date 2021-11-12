4d6fea1074b55bd90e2369fb590721ed01ea393a
Arjunpradeep993 <94111048+Arjunpradeep993@users.noreply.github.com>
2021-11-12 11:50:06 +0530

    Update __init__.py

--- LuciferMoringstar_Robot/__init__.py
+++ LuciferMoringstar_Robot/__init__.py
@@ -16,26 +16,24 @@ Basic Commads
 /start :- check am Alive
 
 Bot Owner Only
-/broadcast Replay Any Message or Media
-/stats user Status
-/ban_user  click ban_user more info
-/unban_user click unban_user more info
-/banned_users Banned User Details
-/total How Many Files Added In Database
-/logger  Get Logs
-/delete Delete File From Database
+/broadcast - Replay Any Message or Media
+/stats - user Status
+/ban_user - click ban_user more info
+/unban_user - Unban a banned user
+/banned_users -Banned User Details
+/total - How Many Files Added In Database
+/logger - Get Logs
+/delete - Delete File From Database
 
 """
 
 ABOUT = """
-➥ Name :  Auto Filter V3
+➥ Name :  LAYA
     
-➥ Creator : [Naughty PROFESSOR](
-https://youtube.com/c/NaughtyPROFESSOR)
+➥ Creator : Arjun Pradeep,(@Arjunbots)
 
 ➥ Language : Python3
 
 ➥ Library : Pyrogram Asyncio 1.13.0
 
-➥ Tutorial : [Click Me](https://youtu.be/5hnYOKBzyi8)
 """
